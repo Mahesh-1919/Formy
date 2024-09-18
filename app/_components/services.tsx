06cdeb6c975506ad9c9461eb435302b86537b5f1
Mahesh-1919 <maheshjyothula23@gmail.com>
2024-09-19 00:13:20 +0530

    update_space

--- app/_components/services.tsx
+++ app/_components/services.tsx
@@ -74,7 +74,7 @@ export default function ServicesSection() {
           className="flex flex-col items-center justify-center space-y-4 text-center"
         >
           <div className="space-y-2">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-white">
               Our Services
             </h2>
             <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
